7eaac72b58cb26ea15a1023a630f7bbd25901377
Bluemo <shutaro.aoyama@gmail.com>
2020-01-25 12:51:37 +0900

    Created first shader

---
+++ Assets/Shader/ShaderController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.SetColor("_BaseColor", Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
